d2543f7db4d7146febb97e82a872ae7e0e866452
ilya <ilya.lebedev.98@mail.ru>
2021-11-30 02:19:29 +0300

    вычищаем лишнее

--- qrcodeservice/settings.py
+++ qrcodeservice/settings.py
@@ -27,9 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
 
 INSTALLED_APPS = [
@@ -86,10 +83,6 @@ DATABASES = {
         'PORT': '5433',
     }
 }
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
