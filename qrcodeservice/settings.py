b339ba1a89ff23df716e0282788df4a871ac6529
ilya <ilya.lebedev.98@mail.ru>
2021-11-30 00:13:08 +0300

    fffffff

--- qrcodeservice/settings.py
+++ qrcodeservice/settings.py
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'qrcodeservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'oblako',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'HOST' : '127.0.0.1',
+        # 'PORT' : '5433',
     }
 }
 
